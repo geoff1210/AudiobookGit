78808e7e88744a639344354af9ffd091f4398411
geoff1210 <43475712+geoff1210@users.noreply.github.com>
2024-10-03 12:16:22 -0400

    Creation Commit
    YOLO

---
+++ data/temp-data.1727971929078.js
@@ -0,0 +1,1 @@
+window.tempDataJSON = {"books":true,"series":false,"collections":false,"podcasts":false,"wishlist":true,"extras":{"domain-extension":".com","pages":{},"subPageStates":[{"key":"categories","enabled":false},{"key":"authors","enabled":true},{"key":"publishers","enabled":false},{"key":"series","enabled":false},{"key":"narrators","enabled":true}],"cacheID":1727971929078}};
